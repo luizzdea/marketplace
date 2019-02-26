4388ec275fb77bd7aee46586346bb1fcaefce236
Luiz Guilherme Afonso de André <guilherme@MacBook-Air-de-Luiz.local>
2019-02-26 19:15:34 -0300

    Project structure + initial express and routes configuration

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+class App {
+  constructor () {
+    this.express = express()
+    this.isDev =
+      process.hasUncaughtExceptionCaptureCallback.NODE_ENV !== 'production'
+
+    this.middlewares()
+    this.routes()
+  }
+
+  middlewares () {
+    this.express.use(express.json())
+  }
+
+  routes () {
+    this.express.use(require('./routes'))
+  }
+}
+
+module.exports = new App().express
