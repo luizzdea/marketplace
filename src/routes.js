394395b1fee2c8a988d8ede4c8e80c6021025987
Luiz Guilherme Afonso de André <guilherme@MacBook-Air-de-Luiz.local>
2019-03-09 14:42:51 -0300

    validations with joi and express-validation + exceptions

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,51 @@
 const express = require('express')
+const validate = require('express-validation')
+
 const authMiddleware = require('./app/middleware/auth')
+
+const handle = require('express-async-handler')
 
 const controllers = require('./app/controllers')
 
+const validators = require('./app/validators')
+
 const routes = express.Router()
 
-routes.post('/users', controllers.UserController.store)
-routes.post('/sessions', controllers.SessionController.store)
+routes.post(
+  '/users',
+  validate(validators.User),
+  handle(controllers.UserController.store)
+)
+routes.post(
+  '/sessions',
+  validate(validators.Session),
+  handle(controllers.SessionController.store)
+)
 
 routes.use(authMiddleware)
 
 // below this line all routes will apply the middleware
 
-routes.get('/ads', controllers.AdController.index)
-routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
-routes.delete('/ads/:id', controllers.AdController.destroy)
+routes.get('/ads', handle(controllers.AdController.index))
+routes.get('/ads/:id', handle(controllers.AdController.show))
+routes.post(
+  '/ads',
+  validate(validators.Ad),
+  handle(controllers.AdController.store)
+)
+routes.put(
+  '/ads/:id',
+  validate(validators.Ad),
+  handle(controllers.AdController.update)
+)
+routes.delete('/ads/:id', handle(controllers.AdController.destroy))
 
 // Purchase
 
-routes.post('/purchase', controllers.PurchaseController.store)
+routes.post(
+  '/purchase',
+  validate(validators.Purchase),
+  handle(controllers.PurchaseController.store)
+)
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const validate = require('express-validation')
+const Youch = require('youch')
 const dbConfig = require('./config/database')
 
 class App {
@@ -11,6 +13,7 @@ class App {
     this.database()
     this.middlewares()
     this.routes()
+    this.exception()
   }
 
   database () {
@@ -26,6 +29,24 @@ class App {
 
   routes () {
     this.express.use(require('./routes'))
+  }
+
+  exception () {
+    this.express.use(async (err, req, res, next) => {
+      if (err instanceof validate.ValidationError) {
+        return res.status(err.status).json(err)
+      }
+
+      if (process.env.NODE_ENV !== 'production') {
+        const youch = new Youch(err)
+
+        return res.json(await youch.toJSON())
+      }
+
+      return res
+        .status(err.status || 500)
+        .json({ error: 'Internal server error' })
+    })
   }
 }
 
